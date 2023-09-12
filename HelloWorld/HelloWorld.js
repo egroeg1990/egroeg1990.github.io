b40bcaa639675af3d9e85f0748ab2b4e65aaa580
egroeg1990 <144382151+egroeg1990@users.noreply.github.com>
2023-09-12 14:06:59 -0500

    Add files via upload

--- HelloWorld/HelloWorld.js
+++ HelloWorld/HelloWorld.js
@@ -1,5 +1,5 @@
 document.getElementById("heading1").innerHTML="Hello World!"
-document.getElementById("P1").innerHTML="Welcome and thank you for visiting my first webpage!<br>Do not be afraid!<br>This all for an assignment!";
+document.getElementById("P1").innerHTML="Welcome and thank you for visiting my first webpage!<br>Do not be afraid!<br>This is all for an assignment!";
 function msg(){
     alert("Hello world, my tag is Egroeg1990.\nI am a Software Engineering student.\nThis is my first attempt at coding a webpage.");
 }
